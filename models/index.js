da2cbfdc0e5bc487b6344bbf6a6437b69148d538
daniel <daniel@bogusemailserver.com>
2017-02-03 16:47:02 +0000

    Added joi validator.

--- models/index.js
+++ models/index.js
@@ -1,11 +1,40 @@
 'use strict'
 
 const joi = require('joi');
+const uuidV4 = require('uuid/v4');
+const Promise = require('bluebird');
+
+function validateWith(validator,dto) {
+	const promisified = Promise.promisify(validator.validate
+										 ,{context: validator});
+	return promisified(dto);
+}
 
 module.exports.User = class {
-    constructor(email,name,isAdmin) {
-        this.email = email;
-        this.name = name;
-        this.isAdmin = isAdmin;
+    constructor(dto,guid) {
+        this.guid = guid
+        this.email = dto.email;
+        this.name = dto.name;
+        this.isAdmin = dto.isAdmin;
     }
+
+    // https://www.npmjs.com/package/joi
+    static get validator() {
+        return Joi.object().keys({
+			email: Joi.string().alphanum().min(4).max(20).required(),
+			name: Joi.string().alphanum().min(4).max(20).required(),
+			isAdmin: Joi.boolean().required()
+		});
+    }
+
+	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes#Static_methods
+	static validate(dto) { validateWith(this.validator,dto); }
+
+	static validateAndBuild(dto,preexistingGUID) {
+        return this.validate(dto)
+                   .then((dto) => {
+		                const guid = preexistingGUID || uuidV4.uuid();
+                        return new User(dto,guid);
+                   });
+	}
 }
