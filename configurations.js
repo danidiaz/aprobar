ccf7cd655c2ff94a0292b32f005a81d92e72a108
daniel <daniel@bogusemailserver.com>
2017-02-05 22:12:11 +0000

    tabs!

--- configurations.js
+++ configurations.js
@@ -3,25 +3,25 @@
 const mongoAdapter = require('sails-mongo');
 
 module.exports.waterlineConf = (() => {
-	const host = process.env.APROBAR_MONGO_HOST;
+    const host = process.env.APROBAR_MONGO_HOST;
     const db = process.env.APROBAR_MONGO_DB;
     const port = parseInt(process.env.APROBAR_MONGO_PORT);
-	if (isNaN(port)) {
-		throw 'Invalid port format for mongo.'
-	}
-	return { 
-		adapters: { 
-			'sails-mongo': mongoAdapter 
-		},
-		connections: {
-			mongo : {
-				adapter : 'sails-mongo',
-				host: host, 
-				port: port,
-				database: db
-			}
-		}
-	}
+    if (isNaN(port)) {
+        throw 'Invalid port format for mongo.'
+    }
+    return { 
+        adapters: { 
+            'sails-mongo': mongoAdapter 
+        },
+        connections: {
+            mongo : {
+                adapter : 'sails-mongo',
+                host: host, 
+                port: port,
+                database: db
+            }
+        }
+    }
 })();
 
 module.exports.expressConf = (() => {
